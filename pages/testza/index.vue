3c2585492232e1fb7e746bb5224d52cdc42180ed
nikepitchaya <103920925+nikepitchaya@users.noreply.github.com>
2022-07-12 12:35:25 +0700

    product detail slide show with arrow and can click picture below

--- pages/testza/index.vue
+++ pages/testza/index.vue
@@ -1,210 +1,58 @@
 <template>
-  <div class="bg w-full relative px-24 lg:px-12 xl:px-20 2xl:px-36">
-    <VueSlickCarousel class="h-full flex justify-center items-center" v-bind="settings">
-
-      <div>
-        <div class="bg flex justify-center items-start mt-24">
-          <div class="bg3 flex h-full items-center justify-between">
-            <div class="w-1/2 h-full flex items-center justify-center">
-              <div class="flex h-full flex-col items-start justify-center">
-                <h1 class="text-4xl 2xl:text-6xl ml-8 text-quaternary">Canabliss Oasiz</h1>
-                <h2 class="mt-2 ml-8 text-3xl 2xl:text-6xl text-primary font-medium">Red Fruit Overnight Mask</h2>
-                <div class="mt-4 xl:mt-8 w-full flex items-center">
-                  <div class="h-24 xl:h-28 2xl:h-36 w-3 2xl:w-4 bg-primary mr-8"></div>
-                  <p class="text-xl xl:text-2xl 2xl:text-4xl font-light text-quaternary">Lorem ipsum dolor sit amet ,
-                    consectetur adipiscing elit
-                    ,
-                    sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua .</p>
-                </div>
-                <div class="w-full mt-20 flex items-center hidden">
-                  <div class="h-28 w-28 mr-8 border-2 border-tertiary rounded-full flex items-center justify-center">
-                    <div class="h-24 w-24 bg-tertiary rounded-full flex items-center justify-center">
-                      <base-icon icon="play" viewBox="0 0 30 41" size="30" class="text-white" />
-                    </div>
-                  </div>
-                  <p class="text-3xl text-quaternary">Watch the video</p>
-                </div>
-              </div>
-            </div>
-            <div class="w-1/2 flex justify-center">
-              <img class="w-full" src="~/static/images/IMG_06fav/Group1063@2x.png" alt="">
-            </div>
-          </div>
-        </div>
+  <div class="w-96 h-96 bg-red-500">
+    <div class="flex items-center">
+      <div @click="arrow_left">
+        <base-icon icon='dropdown' viewBox="0 0 24 24" class="transform rotate-90" size="18" />
       </div>
-      <div>
-        <div class="bg flex justify-center items-start mt-24">
-          <div class="bg3 bg-white flex h-full items-center justify-between">
-            <div class="w-1/2 h-full flex items-center justify-center">
-              <div class="flex h-full flex-col items-start justify-center">
-                <h1 class="text-4xl 2xl:text-6xl ml-8 text-quaternary">Canabliss Oasiz</h1>
-                <h2 class="mt-2 ml-8 text-3xl 2xl:text-6xl text-primary font-medium">Red Fruit Overnight Mask</h2>
-                <div class="mt-4 xl:mt-8 w-full flex items-center">
-                  <div class="h-24 xl:h-28 2xl:h-36 w-3 2xl:w-4 bg-primary mr-8"></div>
-                  <p class="text-xl xl:text-2xl 2xl:text-4xl font-light text-quaternary">Lorem ipsum dolor sit amet ,
-                    consectetur adipiscing elit
-                    ,
-                    sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua .</p>
-                </div>
-                <div class="w-full mt-20 flex items-center hidden">
-                  <div class="h-28 w-28 mr-8 border-2 border-tertiary rounded-full flex items-center justify-center">
-                    <div class="h-24 w-24 bg-tertiary rounded-full flex items-center justify-center">
-                      <base-icon icon="play" viewBox="0 0 30 41" size="30" class="text-white" />
-                    </div>
-                  </div>
-                  <p class="text-3xl text-quaternary">Watch the video</p>
-                </div>
-              </div>
-            </div>
-            <div class="w-1/2 flex justify-center">
-              <img class="w-full" src="~/static/images/IMG_06fav/Group1063@2x.png" alt="">
-            </div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="bg flex justify-center items-start mt-24">
-          <div class="bg3 flex h-full items-center justify-between">
-            <div class="w-1/2 h-full flex items-center justify-center">
-              <div class="flex h-full flex-col items-start justify-center">
-                <h1 class="text-4xl 2xl:text-6xl ml-8 text-quaternary">Canabliss Oasiz</h1>
-                <h2 class="mt-2 ml-8 text-3xl 2xl:text-6xl text-primary font-medium">Red Fruit Overnight Mask</h2>
-                <div class="mt-4 xl:mt-8 w-full flex items-center">
-                  <div class="h-24 xl:h-28 2xl:h-36 w-3 2xl:w-4 bg-primary mr-8"></div>
-                  <p class="text-xl xl:text-2xl 2xl:text-4xl font-light text-quaternary">Lorem ipsum dolor sit amet ,
-                    consectetur adipiscing elit
-                    ,
-                    sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua .</p>
-                </div>
-                <div class="w-full mt-20 flex items-center hidden">
-                  <div class="h-28 w-28 mr-8 border-2 border-tertiary rounded-full flex items-center justify-center">
-                    <div class="h-24 w-24 bg-tertiary rounded-full flex items-center justify-center">
-                      <base-icon icon="play" viewBox="0 0 30 41" size="30" class="text-white" />
-                    </div>
-                  </div>
-                  <p class="text-3xl text-quaternary">Watch the video</p>
-                </div>
-              </div>
-            </div>
-            <div class="w-1/2 flex justify-center">
-              <img class="w-full" src="~/static/images/IMG_06fav/Group1063@2x.png" alt="">
-            </div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <div class="bg flex justify-center items-start mt-24">
-          <div class="bg3 bg-white flex h-full items-center justify-between">
-            <div class="w-1/2 h-full flex items-center justify-center">
-              <div class="flex h-full flex-col items-start justify-center">
-                <h1 class="text-4xl 2xl:text-6xl ml-8 text-quaternary">Canabliss Oasiz</h1>
-                <h2 class="mt-2 ml-8 text-3xl 2xl:text-6xl text-primary font-medium">Red Fruit Overnight Mask</h2>
-                <div class="mt-4 xl:mt-8 w-full flex items-center">
-                  <div class="h-24 xl:h-28 2xl:h-36 w-3 2xl:w-4 bg-primary mr-8"></div>
-                  <p class="text-xl xl:text-2xl 2xl:text-4xl font-light text-quaternary">Lorem ipsum dolor sit amet ,
-                    consectetur adipiscing elit
-                    ,
-                    sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua .</p>
-                </div>
-                <div class="w-full mt-20 flex items-center hidden">
-                  <div class="h-28 w-28 mr-8 border-2 border-tertiary rounded-full flex items-center justify-center">
-                    <div class="h-24 w-24 bg-tertiary rounded-full flex items-center justify-center">
-                      <base-icon icon="play" viewBox="0 0 30 41" size="30" class="text-white" />
-                    </div>
-                  </div>
-                  <p class="text-3xl text-quaternary">Watch the video</p>
-                </div>
-              </div>
-            </div>
-            <div class="w-1/2 flex justify-center">
-              <img class="w-full" src="~/static/images/IMG_06fav/Group1063@2x.png" alt="">
-            </div>
-          </div>
-        </div>
-      </div>
-
-    </VueSlickCarousel>
-    <div class="contact right-12 xl:right-24 2xl:right-36">
-      <div class="flex space-x-8">
-        <img class="w-8 2xl:w-10" src="~/static/images/IMG_01home/pages_01home/icons8-facebook@2x.png" alt="">
-        <img class="w-8 2xl:w-10" src="~/static/images/IMG_01home/pages_01home/icons8-instagram@2x.png" alt="">
-        <a href="https://lin.ee/i5MkM6w" target="_blank">
-          <img class="w-8 2xl:w-10" src="~/static/images/IMG_01home/pages_01home/icons8-line@2x.png" alt="">
-        </a>
-        <img class="w-8 2xl:w-10" src="~/static/images/IMG_01home/pages_01home/icons8-twitter@2x.png" alt="">
+      <img :src="images[current]" class="w-48 h-48" alt="product images">
+      <div @click="arrow_right">
+        <base-icon icon='dropdown' viewBox="0 0 24 24" class="transform -rotate-90" size="18" />
       </div>
     </div>
-    <!-- <div class="w-full h-1/6 flex justify-between items-start">
-      <div class="w-h-24 xl:full">
-        <p class="text-xl xl:text-2xl 2xl:text-4xl text-primary"><span class="font-bold">1</span>/4</p>
-        <div class="progres mt-7 bg-primary rounded-full h-0.5 flex items-center">
-          <div class="bg-primary h-1.5 rounded-full" style="width: 25%"></div>
-        </div>
-      </div>
-      <div class="flex space-x-12 mt-12">
-        <a href=""><img class="w-12" src="~/static/images/IMG_01home/pages_01home/icons8-facebook@2x.png" alt=""></a>
-        <a href=""><img class="w-12" src="~/static/images/IMG_01home/pages_01home/icons8-instagram@2x.png" alt=""></a>
-        <a href=""><img class="w-12" src="~/static/images/IMG_01home/pages_01home/icons8-line@2x.png" alt=""></a>
-        <a href=""><img class="w-12" src="~/static/images/IMG_01home/pages_01home/icons8-twitter@2x.png" alt=""></a>
-      </div>
-    </div> -->
-
+    <div class="flex w-full overflow-x-auto">
+      <img v-for="(item, i) in images" :key="i" :src="images[i]" @click="current = i" class="w-20 h-20"
+        alt="product images" :class="{ 'border-4 border-blue-500': i == current }">
+    </div>
   </div>
 </template>
 
 <script>
-import VueSlickCarousel from 'vue-slick-carousel'
-import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-// optional style for arrows & dots
-// import './css/carousel.css'
 export default {
-  components: { VueSlickCarousel },
   data() {
     return {
-      settings: {
-        dots: true,
-        arrows: false,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 6000,
-        pauseOnDotsHover: false,
-        pauseOnFocus: false,
-        pauseOnHover: false,
-      },
+      current: 0,
+      images: [
+        'https://placekitten.com/801/800',
+        'https://placekitten.com/802/800',
+        'https://placekitten.com/803/800',
+        'https://placekitten.com/804/800',
+        'https://placekitten.com/805/800',
+        'https://placekitten.com/806/800',
+        'https://placekitten.com/807/800',
+        'https://placekitten.com/808/800',
+        'https://placekitten.com/809/800',
+        'https://placekitten.com/810/800'
+      ]
     }
   },
+  methods: {
+    arrow_right() {
+      if (this.current >= this.images.length - 1) {
+        this.current = this.images.length - 1
+      }
+      else if (this.current != this.images.length) {
+        this.current++
+      }
+    },
+    arrow_left() {
+      if (this.current <= 0) { this.current = this.current = 0 }
+      else if (this.current != 0) { this.current-- }
+    },
+  }
 }
+
+
 </script>
 
-<style scoped>
-.bg {
-  height: calc(100vh - 84px);
-}
-
-.bg2 {
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-.bg3 {
-  height: 70%;
-}
-
-.vl {
-  height: 145px;
-  width: 15px;
-}
-
-.contact {
-  position: absolute;
-  bottom: 10%;
-}
+<style>
 </style>
