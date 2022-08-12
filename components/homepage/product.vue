064985138b83442a23fc4aba5eaf9806f1b57d09
Ize Phanthakarn <izephanthakarn@hotmail.com>
2022-08-12 18:09:46 +0700

    hide img product in homepage

--- components/homepage/product.vue
+++ components/homepage/product.vue
@@ -43,7 +43,7 @@
             <template v-if="index < page_screen * page && index >= page_screen * (page - 1)">
               <div class="relative mx-2">
                 <img src="~/static/images/IMG_02products_detail/Path357@2x.png" class="" />
-                <img class="centered w-full" :src="require(`~/static/images/products${product.imgUrl[0]}`)" />
+                <img v-if="product.imgUrl[0] != ''" class="centered w-full" :src="require(`~/static/images/products${product.imgUrl[0]}`)" />
                 <span v-if="$day.getDatetoNow(product.release) <= 7"
                   class="px-3 sm:px-9 py-0.5 sm:py-2 3xl:px-6 3xl:py-1.5 text-white text-sm sm:text-base absolute top-3 left-3 sm:top-4 sm:left-4 3xl:top-4 3xl:left-4 bg-primary rounded-full">New</span>
                 <div @click="liked(product.no)"
